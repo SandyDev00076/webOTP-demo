e6cf1885e26281f9b8e4f59a86fb257932ee36d4
sanjeet.tiwari <sanjeet.tiwari@payu.in>
2022-09-02 16:45:28 +0530

    add files

---
+++ index.js
@@ -0,0 +1,31 @@
+console.log("We are going to experiment with WebOTP!!");
+
+// if OTP is supported
+if ("OTPCredential" in window) {
+  // when DOM has loaded
+  window.addEventListener("DOMContentLoaded", (e) => {
+    const input = document.querySelector('input[autocomplete="one-time-code"]');
+    if (!input) return;
+    const ac = new AbortController();
+    const form = input.closest("form");
+    if (form) {
+      form.addEventListener("submit", (e) => {
+        ac.abort();
+        alert("got the OTP and submitted the form!!");
+      });
+    }
+    navigator.credentials
+      .get({
+        otp: { transport: ["sms"] },
+        signal: ac.signal,
+      })
+      .then((otp) => {
+        input.value = otp.code;
+        console.log("OTP received - ", otp.code);
+        if (form) form.submit();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+}
