4671e7ace5944828baf639179a3a3a92bb04844a
sanjeet.tiwari <sanjeet.tiwari@payu.in>
2022-09-06 12:40:08 +0530

    make it functional

--- index.js
+++ index.js
@@ -1,43 +1,63 @@
 console.log("We are going to experiment with WebOTP!!");
 
 // add an event listener to input
-document.querySelector('input').addEventListener('input', (e) => {
+document.querySelector("input").addEventListener("input", (e) => {
   if (e.target.value && e.target.value.length === 6) {
-    document.querySelector('button').disabled = false;
+    document.querySelector("button").disabled = false;
   } else {
-    document.querySelector('button').disabled = true;
+    document.querySelector("button").disabled = true;
   }
-})
+});
 
-// if OTP is supported
-if ("OTPCredential" in window) {
-  // when DOM has loaded
-  window.addEventListener("DOMContentLoaded", (e) => {
-    const input = document.querySelector('input[autocomplete="one-time-code"]');
-    if (!input) return;
+// creating a function that can be modularized
+function addOTPfunctionality(
+  inputElement,
+  onOTPReceiveSuccess,
+  onOTPReceiveFail
+) {
+  // if OTP is supported
+  if ("OTPCredential" in window) {
+    // a variable for abort controller which will be returned
     const ac = new AbortController();
-    const form = input.closest("form");
-    if (form) {
-      form.addEventListener("submit", (e) => {
-        e.preventDefault();
-        ac.abort();
-        alert("got the OTP and submitted the form!!");
-      });
-    } else {
-        console.error('form not found');
-    }
-    navigator.credentials
-      .get({
-        otp: { transport: ["sms"] },
-        signal: ac.signal,
-      })
-      .then((otp) => {
-        input.value = otp.code;
-        document.querySelector('button').disabled = false;
-        console.log("OTP received - ", otp.code);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+
+    // when DOM has loaded
+    window.addEventListener("DOMContentLoaded", (e) => {
+      if (!inputElement) return;
+      navigator.credentials
+        .get({
+          otp: { transport: ["sms"] },
+          signal: ac.signal,
+        })
+        .then((otp) => {
+          inputElement.value = otp.code;
+          onOTPReceiveSuccess(otp);
+        })
+        .catch((err) => {
+          onOTPReceiveFail(err);
+        });
+    });
+    return ac;
+  } else return undefined;
+}
+
+const ac = addOTPfunctionality(
+  document.querySelector('input[autocomplete="one-time-code"]'),
+  (otp) => {
+    document.querySelector("button").disabled = false;
+    console.log("OTP received - ", otp.code);
+  },
+  (err) => {
+    console.error(err);
+  }
+);
+
+const form = inputElement.closest("form");
+if (form) {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    ac.abort();
+    alert("got the OTP and submitted the form!!");
   });
+} else {
+  console.error("form not found");
 }
