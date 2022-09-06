ce5d0176d72e166802c778a53f4dda0d500a09cc
sanjeet.tiwari <sanjeet.tiwari@payu.in>
2022-09-06 13:04:51 +0530

    add a timer as well

--- index.js
+++ index.js
@@ -9,16 +9,33 @@ document.querySelector("input").addEventListener("input", (e) => {
   }
 });
 
-// creating a function that can be modularized
+/**
+ * Modularized function for listening to OTP
+ * @param {HTMLElement} inputElement Element which we need to fill after OTP is received
+ * @param {Function} onOTPReceiveSuccess Function to be called when OTP has been successfully received
+ * @param {Function} onOTPReceiveFail Function to be called when OTP cannot be retreived
+ * @param {number} duration Duration till which we want to listen for OTP (in seconds)
+ * @returns An abort controller which you can abort if you want to stop listening for OTP
+ */
 function addOTPfunctionality(
   inputElement,
   onOTPReceiveSuccess,
-  onOTPReceiveFail
+  onOTPReceiveFail,
+  duration,
+  afterTimerExpiry
 ) {
   // if OTP is supported
   if ("OTPCredential" in window) {
     // a variable for abort controller which will be returned
     const ac = new AbortController();
+
+    // check for duration completion
+    if (duration && typeof duration === "number") {
+      setTimeout(() => {
+        ac.abort();
+        if (afterTimerExpiry) afterTimerExpiry();
+      }, duration * 1000);
+    }
 
     // when DOM has loaded
     window.addEventListener("DOMContentLoaded", (e) => {
@@ -48,6 +65,15 @@ const ac = addOTPfunctionality(
   },
   (err) => {
     console.error(err);
+  },
+  60,
+  () => {
+    const tip = document.createElement("h3");
+    tip.style.marginTop = "16px";
+    tip.style.color = "#1565c0";
+    tip.style.textAlign = "center";
+    tip.innerText = "Please manually enter the OTP";
+    document.querySelector(".panel").appendChild(tip);
   }
 );
 
